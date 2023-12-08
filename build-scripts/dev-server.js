9b85dc1245d222afae922e3e1488fd771d593d6f
Rogerio Romao <58592935+rogerio-romao@users.noreply.github.com>
2023-12-08 07:54:55 +0000

    Add express-rate-limit middleware

--- build-scripts/dev-server.js
+++ build-scripts/dev-server.js
@@ -1,5 +1,6 @@
 /* eslint-disable no-console */
 import express from "express";
+import { rateLimit } from "express-rate-limit";
 import path from "node:path";
 import open from "open";
 import { fileURLToPath } from "url";
@@ -11,8 +12,15 @@ const port = 3000;
 
 const app = express();
 
-// security best practice
+// security best practices
 app.disable("x-powered-by");
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+    standardHeaders: "draft-7", // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
+    legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+    // store: ... , // Use an external store for consistency across multiple server instances.
+});
 
 const compiler = webpack(config);
 
@@ -22,12 +30,12 @@ app.use(
     })
 );
 
-app.get("/", (req, res) => {
+app.get("/", limiter, (_req, res) => {
     const filename = fileURLToPath(import.meta.url);
     res.sendFile(path.join(path.dirname(filename), "../src/index.html"));
 });
 
-app.get("/users", (req, res) => {
+app.get("/users", (_req, res) => {
     res.json([
         { id: 1, firstName: "Bob", lastName: "Smith", email: "bob@gmail.com" },
         {
@@ -46,9 +54,6 @@ app.get("/users", (req, res) => {
 });
 
 app.listen(port, (err) => {
-    if (err) {
-        console.error(err);
-    } else {
-        open("http://localhost:" + port);
-    }
+    if (err) console.error(err);
+    else open("http://localhost:" + port);
 });
